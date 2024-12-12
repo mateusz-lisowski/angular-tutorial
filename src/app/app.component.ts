86fa686a3a798232e6981505a82da303673fad39
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-12 23:17:49 +0100

    Implement routing behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgOptimizedImage, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'homes-app';
+  title = 'homes-app-tutorial';
 }
